338eff41ecff5e1fd9f73b277dcb36e0470f6496
Lalit-mahata <lm3654725@gmail.com>
2024-10-10 21:10:26 +0545

    userController

---
+++ api/route/userRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { createUser, deleteUserById,getAllUsers, getUserById, getUserDetails, login, logout, updateCurrentUserProfile, updateUserById } from "../controller/userController.js";
+import { authenticate, authorizeAdmin } from "../middleware/authMiddleware.js"
+const router = express.Router()
+
+router.route("/").post(createUser)
+.get(authenticate, authorizeAdmin,getAllUsers)
+router.route("/login").post(login)
+router.route("/logout").post(authenticate,logout)
+router.route("/:id").get(authenticate,getUserDetails)
+.put(authenticate,updateCurrentUserProfile)
+.delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
+
+export default router
---
+++ api/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import dbConnect from "./config/dbConnect.js";
+const app = express()
+ dotenv.config({path : "api/config/config.env"})
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+import userRoute from "./route/userRoute.js"
+app.use("/api/v2", userRoute)
+app.listen(PORT,()=>{
+    dbConnect()
+    console.log(`server is listening on port : ${PORT}`);
+    
+})
