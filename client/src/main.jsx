72bbd8b7328a87805ac133b031922cf909cf7ac6
Lalit-mahata <lm3654725@gmail.com>
2024-10-14 17:33:03 +0545

    sample

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Provider} from "react-redux"
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
+import Login from "./pages/auth/Login.jsx"
+import Register from './pages/auth/Register.jsx'
+import store from './redux/store.js'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/register' element={<Register/>}/>
+    </Route>
 
+  )
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store} >
+    <RouterProvider router={router} />
+  </Provider>,
 )
