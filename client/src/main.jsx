761149f9ab6e0f963ea67b4c8c5996be17c64558
Lalit-mahata <lm3654725@gmail.com>
2024-10-15 22:18:48 +0545

    categoryList

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute.jsx'
 import Profile from './pages/User/Profile.jsx'
 import AdminRoute from './pages/Admin/AdminRoute.jsx'
 import UserList from './pages/Admin/UserList.jsx'
+import CategoryList from './pages/Admin/CategoryList.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
@@ -26,6 +27,8 @@ const router = createBrowserRouter(
 
     <Route path='/admin' element={<AdminRoute/>}>
     <Route path="userlist" element={<UserList />} />
+    <Route path="categorylist" element={<CategoryList />} />
+
     </Route>
 </Route>
 
---
+++ client/src/redux/api/categoryApiSlice.js
@@ -0,0 +1,34 @@
+import { apiSlice } from "./apiSlice";
+import { CATEGORY_URL } from "../constants";
+
+export const categoryApiSlice = apiSlice.injectEndpoints({
+    endpoints : (builder) =>({
+createCategory : builder.mutation({
+    query : (data)=>({
+        url : `${CATEGORY_URL}`,
+        method : "POST",
+        body : data
+    })
+}),
+updateCategory : builder.mutation({
+    query : ({data,categoryId})=>({
+url : `${CATEGORY_URL}/${categoryId}`,
+method : "PUT",
+body : data
+    })
+}),
+deleteCatewgory : builder.mutation({
+    query : (categoryId)=>({
+        url : `${CATEGORY_URL}/${categoryId}`,
+        method : "DELETE"
+    })
+}),
+categories : builder.query({
+    query : () =>({
+        url :`${CATEGORY_URL}/categories`
+    })
+})
+    })
+})
+
+export const {useCreateCategoryMutation,useUpdateCategoryMutation,useDeleteCatewgoryMutation,useCategoriesQuery} = categoryApiSlice
